ec343b915159d62d52204587e9b4e468fed2956f
Rojs Smits <97155747+RojsSmits@users.noreply.github.com>
2022-02-10 06:32:54 +0000

    .

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ telpas=["Valmiera, Kaktusa iela, 34","Cēsis, Poda iela, 12","Valmiera, desu iel
 darbalaiki=["8:30-20:00","9:00-21:00","8:00-22:00"]
 bildes=["https://www.baseins.eu/wp-content/uploads/2013/04/6X4B8217.jpg","https://www.baseins.eu/wp-content/uploads/2013/04/6X4B8217.jpg","https://www.baseins.eu/wp-content/uploads/2013/04/6X4B8217.jpg"]
 aprikojums=["divas 10 kg hanteles","25 kg svaru bumba","12 matrači, 2 skriešanas trenežieri"]
-abonetaji=["vards: juris telefona numurs vai ēpasts: 98765432 aboments: 1 treniņš nedēļā: 5 eiro "]
 reitingu_skaits=[2,2,2,2,2,2]
 reitings=[9,10,8,6,9,2]
 abonesanas_skaits=[3,4,5,2]
-abomenti=["1 treniņš nedēļā: 5 eiro","2 treniņi nedēļā: 10 eiro","4 treniņi nedēļā: 20 eiro","7 treniņi nedēļā: 30 eiro"]
-abonetaji=["tel. num., personas kods:5720 aboments:2"]
+abonementi=["1 treniņš nedēļā: 5 eiro","2 treniņi nedēļā: 10 eiro","4 treniņi nedēļā: 20 eiro","7 treniņi nedēļā: 30 eiro"]
+abonetaji=["tel. num., personas kods:5720 abonements:2 treniņi nedēļā: 10 eiro"]
 run=True
 while run==True:
   for i in range(len(reitings)):
@@ -29,11 +28,11 @@ while run==True:
         if v_parbaud==treneri_v[i] and pin_parbaud==paroles[i]:
           print("Sveiki teneri/e!")
           atbilde=("nē")
-          apraksts_jaut=str(input("Vai, vējaties pārmainīt savu   aprakstu?:"))
+          apraksts_jaut=str(input("Vai, vēlaties pārmainīt savu   aprakstu?:"))
           if apraksts_jaut==("jā"):
             apraksts_main=str(input("Rakstat pilnu aprakstu ar jūsu gribētājām pārmaiņām:"))
             treneris_apraksts[i]=apraksts_main
-          pieejams_jaut=str(input("Vai gribat pārmainīt savas   darbadienas un laikus?: "))
+          pieejams_jaut=str(input("Vai gribat pārmainīt savas   darba dienas un laikus?: "))
           if pieejams_jaut==("jā"):
             pieejams_main=str(input("Ievadiet savus pārmainītos   darba laikus, dienas: "))
             treneris_pieejams[i]=pieejams_main
@@ -53,27 +52,28 @@ while run==True:
   elif apmekletajs==("apmeklētājs"):      
     velme=str(input("vai gribi trenēties viens, vai ar treneri?: "))
     if velme=="treneri" or velme=="ar treneri":
-      bridinajums=str(input("(lai pieteiktos pie trenera tev jabūt abonētājam)Vai tu esi abonetajs?:"))
+      bridinajums=str(input("(lai pieteiktos pie trenera tev jabūt abonētājam)Vai Jūs esi abonētajs?:"))
       if bridinajums==("jā"):
         while jaut==("jā"):
           telefons=str(input("ierakstiet savu personas kodu un telefona numuru:"))
-          print(abomenti)
-          abonements_pieteikt=(int(input("ievadiet abomenta kārtas ciparu ,skatoties no kreisāspuses (ar cipariem): ")))
+          print(abonementi)
+          abonements_pieteikt=(int(input("ievadiet abonementa kārtas ciparu ,skatoties no kreisāspuses (ar cipariem): ")))
           for i in range(len(abonetaji)):
-            if abonetaji[i]==(f"tel. num., personas kods:{telefons} aboments:{abomenti[abonements_pieteikt-1]}"):
+            if abonetaji[i]==(f"tel. num., personas kods:{telefons} abonements:{abonementi[abonements_pieteikt-1]}"):
               print("sveiki, ievadītie dati pareizi!")
               jaut=("nē")
+              for i in range (len(paroles)):
+                print(f"treneris/e: {treneri_v[i]} apraksts:   {treneris_apraksts[i]} pieejams: {treneris_pieejams[i]} reitings:{zvaigznes[i]}")
+              novertesana=str(input("vai gribat novērtēt treneri?: "))
+              if novertesana==("jā"):
+                karta=int(input(f"kuru pēckārtas skatoties no kreisās malas sarakstā {treneri_v} treneri jūs gribat novērtēt (rakstat ar skaitļiem bez punkta) "))
+                reitings[karta-1]+=int(input("ievadi zvaigžņu skaitu 1-5 veselos skaitļos?: "))
+                reitingu_skaits[karta-1]+=1
               break
+              jaut==("nē")
             if i==len(abonetaji)-1:
               print("Ievadīti nepareizi dati.")
               jaut=str(input("vai mēģināsiet velreiz?: "))
-        for i in range (len(paroles)):
-          print(f"treneris/e: {treneri_v[i]} apraksts:   {treneris_apraksts[i]} pieejams: {treneris_pieejams[i]} reitings:{zvaigznes[i]}")
-          novertesasa=str(input("vai gribat novērtēt treneri?: "))
-          if novertesasa==("jā"):
-            karta=int(input(f"kuru pēckārtas skatoties no kreisās malas sarakstā {treneri_v} treneri jūs gribat novērtēt (rakstat ar skaitļiem bez punkta) "))
-            reitings[karta-1]+=int(input("ievadi zvaigžņu skaitu 1-5 veselos skaitļos?: "))
-            reitingu_skaits[karta-1]+=1
     else:
       telpas_izveles=str(input("vai gribi redzēt piedāvātās zāles un to darba laikus?: "))
       if telpas_izveles==("jā"):
@@ -87,22 +87,22 @@ while run==True:
       if aprikojums_izvele==("jā"):
         for i in range (len(aprikojums_izvele)):
           print(f"telpas:{telpas[i]} aprikojums: {aprikojums[i]}")
-      aboments_jaut=str(input("vai gribat pieteikties uz treniņu?: "))
-      if aboments_jaut==("jā"):
-        print(abomenti)
+      abonements_jaut=str(input("vai gribat pieteikties uz treniņu?: "))
+      if abonements_jaut==("jā"):
+        print(abonementi)
         telefons=str(input("ierakstiet savu personas kodu un telefona numuru:"))
-        abonements_pieteikt=(int(input("ievadiet abomenta kārtas ciparu ,skatoties no kreisāspuses (ar cipariem): ")))
+        abonements_pieteikt=(int(input("ievadiet abonementa kārtas ciparu ,skatoties no kreisāspuses (ar cipariem): ")))
         abonesanas_skaits[abonements_pieteikt-1]+=1
-        abonetaji.append(f"tel. num., personas kods:{telefons} aboments:{abomenti[abonements_pieteikt-1]}")
-      aboments_atcelt=str(input("vai gribat atcelt abomentu?: "))
-      if aboments_atcelt==("jā"):
+        abonetaji.append(f"tel. num., personas kods:{telefons} abonements:{abonementi[abonements_pieteikt-1]}")
+      abonements_atcelt=str(input("vai gribat atcelt abonementu?: "))
+      if abonements_atcelt==("jā"):
         telefons=str(input("ierakstiet savu personas kodu un telefona numuru:"))
-        print(abomenti)
-        abonements_pieteikt=(int(input("ievadiet abomenta kārtas ciparu ,skatoties no kreisāspuses (ar cipariem): ")))
+        print(abonementi)
+        abonements_pieteikt=(int(input("ievadiet abonementa kārtas ciparu ,skatoties no kreisāspuses (ar cipariem): ")))
         for i in range(len(abonetaji)):
-          if abonetaji[i]==(f"tel. num., personas kods:{telefons} aboments:{abomenti[abonements_pieteikt-1]}"):
-            abonesanas_skaits[abonements_pieteikt-1]-=1
-            abonetaji.remove(f"tel. num., personas kods:{telefons} aboments:{abomenti[abonements_pieteikt-1]}")
+          if abonetaji[i]==(f"tel. num., personas kods:{telefons} abonements:{abonementi[abonements_pieteikt-1]}"):
+            abonesanas_skaits[abonements_pieteikt]-=1
+            abonetaji.remove(f"tel. num., personas kods:{telefons} abonements:{abonementi[abonements_pieteikt-1]}")
   elif apmekletajs==("dunkey32"):
     aprakstu_parb=str(input("vai gribat pārbaudīt treneru aprakstus?: "))
     if aprakstu_parb==("jā"):
@@ -128,10 +128,10 @@ while run==True:
     if treneris_jaut==("jā"):
       treneri_v.append(str(input("ievadiet trenera vārdu uzvārdu: ")))
       paroles=(str(input("ievadi trenera paroli: ")))
-    abonetaji_skaits_jaut=str(input(" katra abomenta veida skaitu?"))
+    abonetaji_skaits_jaut=str(input(" katra abonementa veida skaitu?"))
     if abonetaji_skaits_jaut==("jā"):
-      for i in range (len(abomenti)):
-        print(f"{abomenti[i]} cilvēki pieteikušies: {abonesanas_skaits[i]}")
+      for i in range (len(abonementi)):
+        print(f"{abonementi[i]} cilvēki pieteikušies: {abonesanas_skaits[i]}")
     abonetaji_jaut=str(input(" Vai gribat redzēt abonējošos cilvēkus?"))
     if abonetaji_jaut==("jā"):
       print(abonetaji)
